dddd9d7c379788c6cc4bf0282a838f2d00e8b230
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2023-01-27 08:55:39 -0600

    updated the name of the game

---
+++ guess_the _number.py
@@ -0,0 +1,18 @@
+import random
+# asking player about his name and giving him greetings
+
+
+def player_name():
+    player = input('What is your name?')
+    print(f'welcome {player} to the game of rock-paper-scissor ')
+
+
+# basic stuff before we start the game
+
+
+def basic():
+    print('''
+  1. Computer decides a random number and you have to guess it  
+  2. If your guss 
+  3. Scissor can cut a paper so scissor is stronger than paper so scissor wins 
+  ''')

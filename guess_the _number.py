e472e01a9cd6ff94e4a77f314a6c526a8d05e527
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2023-01-27 09:24:49 -0600

    created the basic of this game

--- guess_the _number.py
+++ guess_the _number.py
@@ -4,7 +4,7 @@ import random
 
 def player_name():
     player = input('What is your name?')
-    print(f'welcome {player} to the game of rock-paper-scissor ')
+    print(f'welcome {player} to the game of Guess The Number ')
 
 
 # basic stuff before we start the game
@@ -13,6 +13,61 @@ def player_name():
 def basic():
     print('''
   1. Computer decides a random number and you have to guess it  
-  2. If your guss 
-  3. Scissor can cut a paper so scissor is stronger than paper so scissor wins 
+  2. If your guess it and if your guess is correct than you win other wise you lose 
+  3. You would only have 10 lives 
   ''')
+
+
+# Choosing the random number
+unknown_number = random.choice(range(0, 101))
+
+# counter of the live
+
+
+# Taking the input from the player
+
+
+def player_choice():
+    choice_again = True
+    while choice_again:
+        choice = (input('Please select a number between 0 to 100: '))
+        if choice.isdigit() == False:
+            print('Please input a number')
+        elif int(choice) not in range(0, 101):
+            print('Please select a number between 0 to 100')
+        elif int(choice) in range(0, 101):
+            choice_again = False
+            return int(choice)
+# play-game
+
+
+def play_game():
+    play = input('Do you want to play the game [Y/N]: ').upper()
+    return play
+# game together
+
+
+def game_together():
+    pg = play_game()
+    if pg == 'Y':
+        player_name()
+        basic()
+        game = True
+        counter = 10
+        while game:
+            pc = player_choice()
+            counter = counter-1
+            print(counter)
+            if pc != unknown_number:
+                player_choice()
+                counter = counter-1
+                print(counter)
+                print('Choose another number')
+
+            else:
+                game = False
+    elif pg == 'N':
+        print('See you later')
+
+
+game_together()
